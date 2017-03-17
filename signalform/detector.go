e8ccfccc576ad63d898e35e4e3e9d6daa3ee2d3b
Francesco Di Chiara <fdc@yelp.com>
2017-03-17 08:50:24 -0700

    Small code refactor

--- signalform/detector.go
+++ signalform/detector.go
@@ -6,15 +6,8 @@ import (
 	"fmt"
 	"github.com/hashicorp/terraform/helper/hashcode"
 	"github.com/hashicorp/terraform/helper/schema"
-	"io/ioutil"
-	"net/http"
 	"sort"
 	"strings"
-)
-
-const (
-	// Workaround for Signalfx bug related to post processing and lastUpdatedTime
-	OFFSET = 10000.0
 )
 
 func detectorResource() *schema.Resource {
@@ -205,31 +198,6 @@ func getNotifications(tf_notifications []interface{}) []map[string]interface{} {
 	}
 
 	return notifications_list
-}
-
-/*
-  Utility function that wraps http calls to SignalFx
-*/
-func sendRequest(method string, url string, token string, payload []byte) (int, []byte, error) {
-	client := &http.Client{}
-
-	req, err := http.NewRequest(method, url, bytes.NewReader(payload))
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("X-SF-Token", token)
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return -1, nil, fmt.Errorf("Failed sending %s request to Signalfx: %s", method, err.Error())
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	defer resp.Body.Close()
-
-	if err != nil {
-		return resp.StatusCode, nil, fmt.Errorf("Failed reading response body from %s request: %s", method, err.Error())
-	}
-
-	return resp.StatusCode, body, nil
 }
 
 /*
---
+++ signalform/util.go
@@ -0,0 +1,38 @@
+package signalform
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const (
+	// Workaround for Signalfx bug related to post processing and lastUpdatedTime
+	OFFSET = 10000.0
+)
+
+/*
+  Utility function that wraps http calls to SignalFx
+*/
+func sendRequest(method string, url string, token string, payload []byte) (int, []byte, error) {
+	client := &http.Client{}
+
+	req, err := http.NewRequest(method, url, bytes.NewReader(payload))
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("X-SF-Token", token)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return -1, nil, fmt.Errorf("Failed sending %s request to Signalfx: %s", method, err.Error())
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+
+	if err != nil {
+		return resp.StatusCode, nil, fmt.Errorf("Failed reading response body from %s request: %s", method, err.Error())
+	}
+
+	return resp.StatusCode, body, nil
+}
